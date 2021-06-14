833d5f09391fa4eddfb6f889310423cd073e4946
Clément Gibaud <git@gibaud.info>
2021-06-14 18:23:20 +0200

    cleanup

--- src/channels.ts
+++
@@ -1,7 +0,0 @@
-import '@feathersjs/transport-commons';
-import { HookContext } from '@feathersjs/feathers';
-import { Application } from './declarations';
-
-export default function(app: Application): void {
-  app.publish(data => app.channel('everybody'));
-}
